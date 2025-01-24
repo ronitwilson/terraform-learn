e4da81e223be6693612c215070990c0c3ab24555
Ronit Wilson <ronit.study@gmail.com>
2025-01-24 23:01:20 +0530

    setup s3 backend

---
+++ s3Backend/s3-setup.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1" # Change this to your desired region
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "ronterrastate240225"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+# Backend configuration for S3 with DynamoDB lock
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucketEncrypt" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+# Create a DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraformstatelocks"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  hash_key = "LockID"
+
+  tags = {
+    Name        = "Terraform State Lock Table"
+    Environment = "Terraform"
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "ronterrastate240225"
+    key            = "terraform/state.tfstate" # Path to the state file
+    region         = "us-east-1"               # AWS region
+    dynamodb_table = "terraformstatelocks"
+    encrypt        = true
+  }
+}
