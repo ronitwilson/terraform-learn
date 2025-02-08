c573e7f87987d1b194749087752ee3ee50099219
Ronit Wilson <ronit.study@gmail.com>
2025-02-08 15:50:30 +0530

    add example for terraform for loop

---
+++ UsingLoops/sample.tf
@@ -0,0 +1,27 @@
+ariable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+  ]
+}
+
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+  vpc_id      = "vpc-0be83fb4blbc4b4f1"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
---
+++ s3Backend/files/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the current date
+current_date=$(date)
+
+# Display the current date
+echo "Today's date is: $current_date"
